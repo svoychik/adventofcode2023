c6b0bd443fb3b4f7b6082d9cff14c69c8eac3095
Serhii Voichyk <svoychik@gmail.com>
2023-12-02 13:20:57 +0100

    Removed not needed

--- Day2/Day2/UnitTest1.cs
+++ Day2/Day2/UnitTest1.cs
@@ -1,18 +1,6 @@
-using System.Text;
 using FluentAssertions;
-
 namespace Day2;
 
-/*
- *
- *  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green | 9blue, 5red, 4 green
-    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
- *
- *
- */
 public enum Color
 {
     Red,
@@ -105,12 +93,12 @@ public class Solution
 
     public int CalculateForPt1(string[] str)
     {
-         return str
+        return str
             .Select(GetMaxAmountForCubes)
-            .Where(x => x.maxBlueUsed <= _blueMax 
-                        && x.maxGreenUsed <= _greenMax 
+            .Where(x => x.maxBlueUsed <= _blueMax
+                        && x.maxGreenUsed <= _greenMax
                         && x.maxRedUsed <= _redMax
-                        )
+            )
             .Sum(x => x.gameId);
     }
 
