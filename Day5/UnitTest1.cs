96ce5b5f31e8e37251af14fc38aa81b85d8c79ec
Serhii Voichyk <svoychik@gmail.com>
2023-12-22 03:02:54 +0100

    Solved day5 pt2

--- Day5/UnitTest1.cs
+++ Day5/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework.Constraints;
 
+// ReSharper disable UseWithExpressionToCopyRecord
+
 namespace Day5;
 
 public class Tests
@@ -28,9 +30,9 @@ public class Tests
     public void It_solves_part2_for_my_personal_input()
     {
         var actualResult = new Solution().SolvePt2(_personalInput);
-        
+
         Console.WriteLine($"The result is {actualResult}");
-        Assert.That(actualResult, Is.EqualTo(46));
+        Assert.That(actualResult, Is.EqualTo(15880236));
     }
 
     [Test]
@@ -44,8 +46,8 @@ public class Tests
     [Test]
     public void It_returns_correct_result_for_pt2_of_example()
     {
-        var actualResult = new Solution().SolvePt2(_personalInput);
-        
+        var actualResult = new Solution().SolvePt2(_exampleInput);
+
         Console.WriteLine($"The result is {actualResult}");
         Assert.That(actualResult, Is.EqualTo(46));
     }
@@ -56,27 +58,65 @@ public class Solution
     public long SolvePt2(string personalInput)
     {
         var parseResult = ParseInput(personalInput);
-        List<long> pipelineInputs = parseResult.Seeds;
+        var ranges = parseResult.SeedsForPt2;
         foreach (var maps in parseResult.Pipelines)
         {
-            var pipelineOutput = new List<long>();
-            foreach (var num in pipelineInputs)
+            var input = new Queue<Range>(ranges);
+            var output = new List<Range>();
+
+            while (input.Any())
             {
-                var map = maps.SingleOrDefault(x => x.Src.Start <= num && num <= x.Src.End);
+                var range = input.Dequeue();
+                var map = maps.FirstOrDefault(src => Intersects(src.Src, range));
+
+                /*
+                  1. If map not found - add it to the output
+                  2. If range is fully covered by map - add it the output
+                3-4. Otherwise chop the range into two depending on the range intersection
+                 
+                 
+                 */
                 if (map == null)
                 {
-                    pipelineOutput.Add(num);
+                    output.Add(range);
+                    continue;
+                }
+
+                if (map.Src.Start <= range.Start && range.End <= map.Src.End)
+                {
+                    var shift = map.Dst.Start - map.Src.Start;
+                    output.Add(new Range(range.Start + shift, range.End + shift));
                 }
                 else
                 {
-                    var distanceFromStart = num - map.Src.Start;
-                    pipelineOutput.Add(map.Dst.Start + distanceFromStart);
+                    // Determine the overlap and non-overlap parts with the map source.
+                    long overlapStart = Math.Max(range.Start, map.Src.Start);
+                    long overlapEnd = Math.Min(range.End, map.Src.End);
+
+                    // Enqueue the non-overlapping part before the overlap (if it exists).
+                    if (overlapStart > range.Start)
+                    {
+                        input.Enqueue(new Range(range.Start, overlapStart - 1));
+                    }
+
+                    // Enqueue the overlapping part.
+                    input.Enqueue(new Range(overlapStart, overlapEnd));
+
+                    // Enqueue the non-overlapping part after the overlap (if it exists).
+                    if (overlapEnd < range.End)
+                    {
+                        input.Enqueue(new Range(overlapEnd + 1, range.End));
+                    }
                 }
             }
-            pipelineInputs = pipelineOutput;
+
+            ranges = output;
         }
-        return pipelineInputs.Min();
+
+        return ranges.MinBy(x => x.Start).Start;
     }
+
+    bool Intersects(Range r1, Range r2) => r1.Start <= r2.End && r2.Start <= r1.End;
 
     public long SolvePt1(string personalInput)
     {
@@ -98,8 +138,10 @@ public class Solution
                     pipelineOutput.Add(map.Dst.Start + distanceFromStart);
                 }
             }
+
             pipelineInputs = pipelineOutput;
         }
+
         return pipelineInputs.Min();
     }
 
@@ -118,17 +160,12 @@ public class Solution
             .Select(long.Parse)
             .ToArray();
 
-        var seedsForPt2 = new List<long>();
+        var seedsRanges = new List<Range>();
         for (var j = 0; j < seedsAndRanges.Length; j += 2)
         {
-            var currCount = 1;
             long curr = seedsAndRanges[j];
-            long max = seedsAndRanges[j + 1];
-            seeds.Add(curr);
-            while (currCount < max)
-            {
-                seedsForPt2.Add(++curr);
-            }
+            long times = seedsAndRanges[j + 1];
+            seedsRanges.Add(new Range(curr, curr + times - 1));
         }
 
         var pipelinesMaps = new List<Map>[7];
@@ -154,12 +191,12 @@ public class Solution
             i++;
         }
 
-        return new ParseResult(seeds, seedsForPt2, pipelinesMaps);
+        return new ParseResult(seeds, seedsRanges, pipelinesMaps);
     }
-
-    
 }
 
 public record Range(long Start, long End);
+
 public record Map(Range Src, Range Dst);
-public record ParseResult(List<long> Seeds, List<long> SeedsForPt2, List<Map>[] Pipelines);
+
+public record ParseResult(List<long> Seeds, List<Range> SeedsForPt2, List<Map>[] Pipelines);
